d286cf7d3804d495047bc26289d6b170fe8ab3b6
zhonya-j <jjya1b2c3bu@gmail.com>
2021-06-12 14:30:32 +0900

    css test app2 push

---
+++ src/App2.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Appbar from './components/Appbar';
+import Cambox from './components/Cambox'
+function App2() {
+  return (
+    <>
+      <Appbar></Appbar>
+      <Cambox></Cambox>
+    </>
+  );
+}
+
+export default App2;
