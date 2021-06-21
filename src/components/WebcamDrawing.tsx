7665b31c63d873193b35df4802f15812f43c9db7
simon-hoon <ropeiny@gmail.com>
2021-06-21 23:31:22 +0900

    ADD color changes & Implement mobile-size version

--- src/components/WebcamDrawing.tsx
+++ src/components/WebcamDrawing.tsx
@@ -1,5 +1,4 @@
-
-import { useRef, useState, useEffect, useCallback} from 'react';
+import { useRef, useState, useEffect, useCallback, useMemo} from 'react';
 import axios from 'axios';
 import { useMutation } from 'react-query';
 import Webcam from 'react-webcam';
@@ -14,6 +13,7 @@ import { red, pink, purple, deepPurple, indigo, blue,
 
 // d3
 import { select } from "d3";
+
 interface maskData {
     check : boolean
     bboxes : number[][],
@@ -44,10 +44,11 @@ const initData : maskData ={
   'segmentations' : ['0,0']
 }
 
-// 10 colors
+// 16 colors
 const initColors :string[] = [
-  red[400], purple[400], indigo[400], blue[400], cyan[400], 
-  green[400], lime[400], yellow[400], amber[400], deepOrange[400]
+  red[400], pink[400], purple[400], deepPurple[400], indigo[400], blue[400],
+  lightBlue[400], cyan[400], teal[400], green[400], lightGreen[400],
+  lime[400], yellow[400], amber[400], orange[400], deepOrange[400]
 ]
 
 const videoConstraints = {
@@ -61,23 +62,24 @@ const WebcamDrawing = (props : toggles) => {
     const svgRef = useRef(null);
     const webcamRef = useRef<any>(null);
     const [previewURL, setPreview] = useState<string>('');
-    const [colors, setColors] = useState<string[]>(initColors) // max colors : 10
+    const [colors, setColors] = useState<string[]>(initColors) // max colors : 16
 
     const [receivedMaskData, setMaskData] = useState<maskData>(initData);
     const [labelData, setLabelData] = useState<string[]>(initData.labels);
     const [bboxData, setBboxData] = useState<number[][]>(initData.bboxes);
     const [segmentation, setSegmentation] = useState<string[]>(initData.segmentations);
 
+    const [windowSize, setWindowSize] = useState<number>(0);
+
     // boostcamp
-    // const api = axios.create({
-    //   baseURL: `http://49.50.165.199:6010/`,
-    // })
+    const api = axios.create({
+        baseURL : `http://49.50.165.199:6010/`,
+    });
 
     // AWS
-    const api = axios.create({
-      baseURL: `http://54.180.91.142/`,
-    })
-
+    // const api = axios.create({
+    //   baseURL: `http://54.180.91.142/`,
+    // })
 
     const [mutateCreate] = useMutation(
         (data: picture) => api.post('masks', data), { 
@@ -93,14 +95,24 @@ const WebcamDrawing = (props : toggles) => {
       }
     })
 
-    // set new 10 colors! 
+    // set new 16 colors! 
     const colorChange = useCallback(() => {
       const newColors : string[] = [];
-      for (let i = 0; i < 10; i++){
+      for (let i = 0; i < 16; i++){
         newColors.push(colorMap[Math.floor(Math.random() * 14)][(Math.floor(Math.random() * 8) + 1)*100])
       }
       setColors(newColors)
     },[setColors]);
+
+    const makeColors = useCallback((n : number) => {
+      const colorNumbers :number[] = [];
+      for (let i = 0 ; i < n; i ++){
+        colorNumbers.push(i)
+      }
+      return colorNumbers;
+    }, []);
+
+    const colorNumbers = useMemo(() => makeColors(16), [makeColors]);
 
     const capture = useCallback(() => {
       const image : string = webcamRef.current.getScreenshot();
@@ -109,6 +121,7 @@ const WebcamDrawing = (props : toggles) => {
 
     const submit = () => {
       const json : string = JSON.stringify({
+        mobile : (windowSize === 224 ? true : false),
         data : previewURL,
       })
       const obj : picture = JSON.parse(json);
@@ -135,17 +148,18 @@ const WebcamDrawing = (props : toggles) => {
           (update) => update.attr("class", "updated"),
           (exit) => exit.remove()
         )
-        .attr('x', (data) => (data[0] - 15))
+        .attr('x', (data) => (data[0] + 5))
         .attr('y', (data) => (data[1] - 5))
         .data(labelData!)
         .text(data => data)
         .attr('font-family', 'Arial')
         .attr('font-size', "11px")
         .attr('text-align', 'center')
-        .attr('fill', 'red')
+        .data(colorNumbers)
+        .attr('fill', (data : number) => colors[data % 5 + 5])
         .attr('stroke-width', 1)
     } else select("text").remove();
-    }, [bboxData, labelData, props.classification]);
+    }, [bboxData, labelData, props.classification, colors, colorNumbers]);
 
     // drawing bounding box
     useEffect(() => {
@@ -160,22 +174,22 @@ const WebcamDrawing = (props : toggles) => {
           (update) => update.attr("class", "updated"),
           (exit) => exit.remove()
         )
-        .attr('width', (data : number[]) => data[2] - data[0])
-        .attr('height', (data : number[]) => data[3] - data[1])
+        .attr('width', (data : number[]) => (data[2] - data[0]))
+        .attr('height', (data : number[]) => (data[3] - data[1]))
         .attr('x', (data : number[]) => data[0])
         .attr('y', (data : number[]) => data[1])
         .attr('fill', 'transparent')
-        .attr('stroke', 'red')
+        .data(colorNumbers)
+        .attr('stroke', (data : number) => colors[9 - data])
         .attr('stroke-width', 2)
       } else select("rect").remove();
-    }, [bboxData, props.detection]);
+    }, [bboxData, props.detection, colors, colorNumbers]);
     
     // drawing segmentation mask
     useEffect(() => {
       if(svgRef.current && props.segmentation){
 
       const svg = select(svgRef.current);
-      const colorNumbers = [0,1,2,3,4,5,6,7,8,9];
 
       svg
         .selectAll("polygon")
@@ -190,8 +204,13 @@ const WebcamDrawing = (props : toggles) => {
         .attr('fill', (data : number) => colors[data])
         .attr('stroke-width', 2)
         .attr('opacity', 0.5)
-      } else select('polygon').remove();
-    }, [segmentation, colors, props.segmentation]);
+    } else select("polygon").remove();
+    }, [segmentation, props.segmentation, colors, colorNumbers]);
+
+     // calculate window size 
+    const windows = useCallback(() => {
+      setWindowSize((window.innerHeight > 640 && window.innerWidth > 640) ? 448 : 224)}, 
+      [setWindowSize]);
 
     // set fps
     useInterval (() =>{
@@ -199,6 +218,7 @@ const WebcamDrawing = (props : toggles) => {
         try{
           capture();
           submit();
+          windows();
         }
         catch (e) {
           console.log(e);
@@ -211,15 +231,16 @@ const WebcamDrawing = (props : toggles) => {
       return () => setPreview('');
     }, []);
 
+
     return (
         <>
           <Grid container justify="center" alignItems="center">
           <Webcam
               audio={false}
-              height={window.innerHeight > 640 ? 448 : 224}
+              height={windowSize}
               ref={webcamRef}
               screenshotFormat="image/jpeg"
-              width={window.innerWidth > 640 ? 448 : 224}
+              width={windowSize}
               videoConstraints={videoConstraints}
           />
           <svg ref={svgRef}
@@ -229,8 +250,8 @@ const WebcamDrawing = (props : toggles) => {
               marginRight: "auto",
               textAlign: "center",
               zIndex: 9,
-              width: window.innerWidth > 640 ? 448 : 224,
-              height: window.innerHeight > 640 ? 448 : 224, 
+              width: windowSize,
+              height: windowSize, 
             }}
           />
           </Grid>
